55902bedd86e34bf0a1098eb4b9ea196e647d006
CodeDotJS <rushforlinux@gmail.com>
2026-08-31 02:06:41 +0530

    feat: let robots index /play

--- apps/web/app/robots.ts
+++ apps/web/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
 	return {
 		rules: {
 			userAgent: "*",
-			allow: ["/", "/about"],
+			allow: ["/", "/about", "/play"],
 			disallow: [
 				"/account",
 				"/admin",
